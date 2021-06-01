4a64d09075b31725a0c3185a38870ed1eb853195
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2021-06-01 10:48:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/video', methods=['GET'])
-def recommend_movies():
+def recommend_videos():
     res = recommendation.results(request.args.get('title'))
     return jsonify(res)
 
 if __name__=='__main__':
     app.run(port = 5000, debug = True)
+
