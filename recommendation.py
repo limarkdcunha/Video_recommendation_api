634398c9a4fb8d61f2ffeef3ccaf06fd585d43ff
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2021-06-01 10:49:07 +0530

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -3,12 +3,13 @@ import scipy.sparse as sp
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#Getting the data from csv file
 def get_data():
-    movie_data = pd.read_csv('dataset/whc-sites-2019.csv')
-    movie_data['name_en'] = movie_data['name_en'].str.lower()
-    return movie_data
+    video_data = pd.read_csv('whc-sites-2019.csv')
+    video_data['name_en'] = video_data['name_en'].str.lower()
+    return video_data
 
-
+#Preprocessing the data as per the needs
 def combine_data(data):
     data_recommend = data.drop(columns=['region_en', 'unique_number','id_no','rev_bis','name_en','short_description_en',
                                 'justification_en','date_inscribed','secondary_dates','danger','date_end','danger_list'
@@ -18,16 +19,11 @@ def combine_data(data):
 
     data_recommend['combine'] = data_recommend[data_recommend.columns[0:3]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
 
-    #print('Hola\n\n\n\n')
-    #print(data_recommend)
-
     data_recommend = data_recommend.drop(columns=[ 'category','states_name_en'])
 
-    #print('\n\n\nHola2\n\n\n\n')
-    #print(data_recommend)
     return data_recommend
 
-
+#Gettting the cosine cosine_similarity
 def transform_data(data_combine, data_plot):
     count = CountVectorizer(stop_words='english')
     count_matrix = count.fit_transform(data_combine['combine'])
@@ -41,8 +37,8 @@ def transform_data(data_combine, data_plot):
 
     return cosine_sim
 
-
-def recommend_movies(title, data, combine, transform):
+#recommendation data that is to be passed to user
+def recommend_videos(title, data, combine, transform):
 
     indices = pd.Series(data.index, index = data['name_en'])
     index = indices[title]
@@ -51,30 +47,34 @@ def recommend_movies(title, data, combine, transform):
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:21]
 
-    movie_indices = [i[0] for i in sim_scores]
+    video_indices = [i[0] for i in sim_scores]
+    #print(video_indices)
 
-    movie_id = data['unique_number'].iloc[movie_indices]
-    movie_title = data['name_en'].iloc[movie_indices]
-    movie_genres = data['category'].iloc[movie_indices]
+    video_id = data['unique_number'].iloc[video_indices]
+    video_title = data['name_en'].iloc[video_indices]
+    video_category = data['category'].iloc[video_indices]
+    video_place = data['states_name_en'].iloc[video_indices]
 
-    recommendation_data = pd.DataFrame(columns=['Unique_number','Name','Category'])
+    recommendation_data = pd.DataFrame(columns=['Unique_number','Name','Category','Location'])
 
-    recommendation_data['Unique_number'] = movie_id
-    recommendation_data['Name'] = movie_title
-    recommendation_data['Category'] = movie_genres
+    recommendation_data['Unique_number'] = video_id
+    recommendation_data['Name'] = video_title
+    recommendation_data['Category'] = video_category
+    recommendation_data['Location'] = video_place
 
     return recommendation_data
 
-def results(movie_name):
-    movie_name = movie_name.lower()
+#Calling function that will be called in this API
+def results(video_name):
+    video_name = video_name.lower()
 
-    find_movie = get_data()
-    combine_result = combine_data(find_movie)
-    transform_result = transform_data(combine_result,find_movie)
+    find_video = get_data()
+    combine_result = combine_data(find_video)
+    transform_result = transform_data(combine_result,find_video)
 
-    if movie_name not in find_movie['name_en'].unique():
+    if video_name not in find_video['name_en'].unique():
         return 'Video Not Found'
 
     else:
-        recommendations = recommend_movies(movie_name, find_movie, combine_result, transform_result)
+        recommendations = recommend_videos(video_name, find_video, combine_result, transform_result)
         return recommendations.to_dict('records')
